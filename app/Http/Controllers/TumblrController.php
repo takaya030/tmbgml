b37383a689e64929270b9f802019082f1210ec34
Minoru Takayama <takaya030@yahoo.co.jp>
2020-05-04 13:31:00 +0900

    add tumblr controller

---
+++ app/Http/Controllers/TumblrController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \Illuminate\Http\Request;
+
+class TumblrController extends Controller
+{
+	public function getPosts( Request $request )
+	{
+		$base_uri = 'https://api.tumblr.com';
+		$client = new \GuzzleHttp\Client([
+			'base_uri' => $base_uri,
+		]);
+
+		$num_mails = env('TUMBLR_MAX_POSTS');		// Gmail にインサートするアイテム数
+
+		$response = $client->request('GET', '/v2/blog/'.env('TUMBLR_USER_ID').'.tumblr.com/posts', [ 'query' => [
+			'api_key'		=> env('TUMBLR_API_KEY'),
+			'limit'			=> $num_mails,
+		]]);
+
+		$response_body = (string)$response->getBody();
+		$result = json_decode( $response_body );
+
+		dd($result);
+	}
+}
