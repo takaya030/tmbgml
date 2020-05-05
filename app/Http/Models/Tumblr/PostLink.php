8341af6997914cf9229bce391853d86db4ee7d35
Minoru Takayama <takaya030@yahoo.co.jp>
2020-05-05 18:39:40 +0900

    add Tumblr link post class

---
+++ app/Http/Models/Tumblr/PostLink.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Models\Tumblr;
+
+/**
+ * Tumblr link post
+ */
+class PostLink extends PostBase
+{
+    /**
+     * @param mixed $post_data [require] A Tumblr post.
+     */
+	public function __construct( $post_data )
+	{
+		parent::__construct( $post_data );
+		$this->parse();
+	}
+
+	protected function parse()
+	{
+		$this->subject = $this->makeSubject( $this->data->type, $this->data->title );
+	}
+
+	protected function getPostBody()
+	{
+		$body = "";
+
+		if( !empty($this->data->source_url) && !empty($this->data->title) )
+		{
+			$body .= '<div><a href="' . $this->data->source_url . '">' . $this->data->title . '</a></div><br />';
+		}
+
+		if( !empty($this->data->description) )
+		{
+			$body .= '<div>' . $this->data->description . '</div><br />';
+		}
+		elseif( !empty($this->data->excerpt) )
+		{
+			$body .= '<div>' . $this->data->excerpt . '</div><br />';
+		}
+
+		return $body;
+	}
+}
