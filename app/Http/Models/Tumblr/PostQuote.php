f58e5f36265d8db12d0f1136bca41d4b22f06985
Minoru Takayama <takaya030@yahoo.co.jp>
2020-05-05 16:37:10 +0900

    add Tumblr quote post class

---
+++ app/Http/Models/Tumblr/PostQuote.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Models\Tumblr;
+
+/**
+ * Tumblr quote post
+ */
+class PostQuote extends PostBase
+{
+    /**
+     * @param mixed $post_data [require] A Tumblr post.
+     */
+	public function __construct( $post_data )
+	{
+		parent::__construct( $post_data );
+		$this->parse();
+	}
+
+	protected function parse()
+	{
+	}
+
+	protected function getPostBody()
+	{
+		$body = "";
+
+		if( !empty($this->data->text) )
+		{
+			$body .= '<div>' . $this->data->text . '</div><br />';
+		}
+
+		if( !empty($this->data->source) )
+		{
+			$body .= '<div>- ' . $this->data->source . '</div><br />';
+		}
+
+		return $body;
+	}
+}
